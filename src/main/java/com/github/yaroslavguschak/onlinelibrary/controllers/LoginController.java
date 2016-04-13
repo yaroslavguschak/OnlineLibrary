d94a7902a7681ee2b0d23a91c389d61e18894d87
Yaroslav Guschak <yar.guschak@gmail.com>
2016-04-13 22:14:47 +0300

    avible if admin checking in header

--- src/main/java/com/github/yaroslavguschak/onlinelibrary/controllers/LoginController.java
+++ src/main/java/com/github/yaroslavguschak/onlinelibrary/controllers/LoginController.java
@@ -51,7 +51,7 @@ public class LoginController {
 
 
     @RequestMapping(value = "/loginchek", method= RequestMethod.POST)
-    @ResponseBody
+//    @ResponseBody
     public void loginCheck(@ModelAttribute("loginRequest") LoginRequest loginRequest, HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException, GeneralSecurityException {
         String login = loginRequest.getLogin();
